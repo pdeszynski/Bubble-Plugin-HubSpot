d7d6760475933ea8afaa32657c736f5ebcfc9e07
Piotr Deszyński <pdeszynski@gmail.com>
2024-02-01 15:04:32 +0100

    Automatic commit from Bubble

---
+++ actions/AEA-852m2/client.js
@@ -0,0 +1,13 @@
+function(properties, context) {
+
+
+    //Load any data 
+
+    var _hsq = window._hsq = window._hsq || [];
+    _hsq.push(["identify",{
+        email: properties.email
+    }]);
+
+    //Do the operation
+
+}
